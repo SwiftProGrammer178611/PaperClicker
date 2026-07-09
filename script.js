7a703f852c5677c984a49bf5168b934d49ad4742
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-09 11:48:22 -0400

    added an pliers upgrade ability and also game works pretty falwlessly. the increasing amoutnf rom each additonal clicker works!

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
+let gpc = 1;
 let gem = document.querySelector('.gem-cost')
-
-let clickerCost = document.querySelector('.clicker-cost')
-
+let clickerCost = document.querySelector('.clicker-cost');
 let parsedGem = parseFloat(gem.innerHTML);
-
 let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let clickerLevel = document.querySelector('.clicker-level')
+let clickerIncrease = document.querySelector('.clicker-increase')
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML);
+
+
+let pliersCost = document.querySelector('.pliers-cost');
+let parsedPliersCost = parseFloat(pliersCost.innerHTML);
+let pliersLevel = document.querySelector('.pliers-level')
+let pliersIncrease = document.querySelector('.pliers-increase')
+let parsedPliersIncrease = parseFloat(pliersIncrease.innerHTML);
+
+
 
 function incrementGem() {
-    parsedGem += 1
-    gem.innerHTML = parsedGem
+    gem.innerHTML = Math.round(parsedGem += gpc);
 }
 
 function buyClicker() {
     if(parsedGem >= parsedClickerCost){
-        parsedGem -= parsedClickerCost
-        gem.innerHTML = parsedGem
+        gem.innerHTML = Math.round(parsedGem-parsedClickerCost);
+
+        clickerLevel.innerHTML ++
+
+        parsedClickerIncrease = parseFloat((parsedClickerIncrease *1.03).toFixed(2));
+        clickerIncrease.innerHTML = parsedClickerIncrease;
+        gpc+= parsedClickerIncrease;
+
+        parsedClickerCost *= 1.18;
+        clickerCost.innerHTML = Math.round(parsedClickerCost);
+
     }
 }
+
+function buyPliers() {
+    if(parsedGem >= parsedPliersCost){
+        gem.innerHTML = Math.round(parsedGem-=parsedPliersCost);
+
+        pliersLevel.innerHTML ++;
+
+        parsedPliersIncrease = parseFloat((parsedPliersIncrease *1.03).toFixed(2));
+        pliersIncrease.innerHTML = parsedPliersIncrease;
+        gpc+= parsedPliersIncrease;
+
+        parsedPliersCost *= 1.18;
+        pliersCost.innerHTML = Math.round(parsedPliersCost);
+
+    }
+}
+
