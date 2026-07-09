e8679015ef173a7c59e1961a3811b494229ec940
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-09 19:04:09 -0400

    save and load mostly work, will need more testing to figure out if there are any errors. Starting to add a scrollbar for upgrades, and cleaning up html code!

--- script.js
+++ script.js
@@ -100,12 +100,47 @@ function buyUpgrade(upgrade) {
     
 }
 
-function save (){
 
+//localStorage being used here bruvs. fancy schmancy
+function save (){
+    localStorage.clear();
+
+    //maps through entire array, and we have to stringify evyerthign bc localstorage only accepts strings
+    upgrades.map((upgrade) => {
+
+        const obj = JSON.stringify({
+            parsedLevel: parseFloat(upgrade.level.innerHTML),
+            parsedCost: upgrade.parsedCost,
+            parsedIncrease: upgrade.parsedIncrease
+        })
+
+        localStorage.setItem(upgrade.name, obj);
+
+    })
+
+    localStorage.setItem('gpc',JSON.stringify(gpc))
+    localStorage.setItem('gps',JSON.stringify(gps))
+    localStorage.setItem('gem',JSON.stringify(gem))
 }
 
 function load() {
-    
+    upgrades.map(upgrade => {
+        const savedValues = JSON.parse(localStorage.getItem(upgrade.name))
+        upgrade.parsedCost = savedValues.parsedCost
+
+        upgrade.parsedIncrease = savedValues.parsedIncrease
+        upgrade.level.innerHTML = savedValues.parsedLevel
+        upgrade.cost.innerHTML = Math.round(upgrade.parsedCost);
+        upgrade.increase.innerHTML = upgrade.parsedIncrease
+
+
+    })
+
+    gpc = JSON.parse(localStorage.getItem('gpc'))
+    gps = JSON.parse(localStorage.getItem('gps'))
+    parsedGem = JSON.parse(localStorage.getItem('parsedGem'))
+
+    gem.innerHTML = Math.round(parsedGem)
 }
 
 //to remove div +1 once the animation is done form the paperClip
